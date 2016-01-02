67ef5ca6bd31b735ef4dcce02f4265b23179b641
CocoLaNoix <corentin.duc@live.fr>
2016-01-02 16:54:33 +0100

    Update rotations.py

--- rotations.py
+++ rotations.py
@@ -1,19 +1,30 @@
+import copy
+
 def rotateright(liste):
     liste = [list(liste) for liste in (zip(*liste[::-1]))]
+    return liste
 
 def rotateleft(liste):
     liste = [list(liste) for liste in (zip(*liste[::-1]))]
     liste = [list(liste) for liste in (zip(*liste[::-1]))]
     liste = [list(liste) for liste in (zip(*liste[::-1]))]
+    return liste
 
 def rotation(liste,cadran,sens):
-    """True c'est rotation vers la droite"""
     if sens == True:
-        """Le 1 désigne le cadran en haut à droite"""
         if cadran == 1:
-            cadran1 = liste.copy()
-            for i in range(len(liste)//2):
-                cadran1.pop(i-len(liste)//2)
-                for x in range(len(cadran1)//2 +1):
-                    cadran1[x].pop(-1)
-                    """Là si tu printes une liste que t'as utilisé en exemple elle est découpée aussi"""
+            pass
+
+liste = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+
+cadran1 = copy.deepcopy(liste)
+
+for i in range(len(liste)//2):
+    cadran1.pop(i-len(liste)//2)
+    for x in range(len(cadran1)//2 +1):
+        cadran1[x].pop(-1)
+
+cadran1 = rotateright(cadran1)
+liste[0][0]
+print(cadran1)
+print (liste)
