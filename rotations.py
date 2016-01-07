7a302e63f4e37aa9948f08ab84084c4acb2fd199
CocoLaNoix <corentin.duc@live.fr>
2016-01-07 20:21:22 +0100

    Update rotations.py

--- rotations.py
+++ rotations.py
@@ -11,108 +11,106 @@ def rotateleft(liste):
     return liste
 
 def rotation(liste,cadran,sens):
+    if sens == True:
 
-    if len(liste) == 4:
-        if sens == True:
+        if cadran == 1:
+            cadrancopy = copy.deepcopy(liste)
+            for i in range(len(liste)//2):
+               cadrancopy.pop(-1)
+            for x in range (len(liste)//2 ):
+                for j in range(len(liste)//2 ):
+                    cadrancopy[x].pop(-1)
+            cadrancopy = rotateright(cadrancopy)
+            for x in range(len(liste)//2):
+                for y in range(len(liste)//2):
+                    liste[x][y] = cadrancopy[x][y]
 
-            if cadran == 1:
-                cadrancopy = copy.deepcopy(liste)
-                for i in range(len(liste)//2):
-                    cadrancopy.pop(i-len(liste)//2)
-                    for x in range(len(cadrancopy)//2 +1):
-                        cadrancopy[x].pop(-1)
-                cadrancopy = rotateright(cadrancopy)
-                liste[0][0] = cadrancopy[0][0]
-                liste[0][1] = cadrancopy[0][1]
-                liste[1][0] = cadrancopy[1][0]
-                liste[1][1] = cadrancopy[1][1]
 
-            if cadran == 2:
-                cadrancopy = copy.deepcopy(liste)
-                for i in range(len(liste)//2):
-                    cadrancopy.pop(i-len(liste)//2)
-                    for x in range(len(cadrancopy)//2 +1):
-                        cadrancopy[x].pop(0)
-                cadrancopy = rotateright(cadrancopy)
-                liste[0][2] = cadrancopy[0][0]
-                liste[0][3] = cadrancopy[0][1]
-                liste[1][2] = cadrancopy[1][0]
-                liste[1][3] = cadrancopy[1][1]
-
-            if cadran == 3:
-                cadrancopy = copy.deepcopy(liste)
-                for i in range(len(liste)//2):
-                    cadrancopy.pop(0)
-                    for x in range(len(cadrancopy)//2 +1):
-                        cadrancopy[x].pop(-1)
-                cadrancopy = rotateright(cadrancopy)
-                liste[2][0] = cadrancopy[0][0]
-                liste[2][1] = cadrancopy[0][1]
-                liste[3][0] = cadrancopy[1][0]
-                liste[3][1] = cadrancopy[1][1]
-
-            if cadran == 4:
-                cadrancopy = copy.deepcopy(liste)
-                for i in range(len(liste)//2 ):
-                    cadrancopy.pop(0)
-                for x in range(len(liste)//2):
+        if cadran == 2:
+            cadrancopy = copy.deepcopy(liste)
+            for i in range(len(liste)//2):
+               cadrancopy.pop(-1)
+            for x in range (len(liste)//2 ):
+                for j in range(len(liste)//2 ):
                     cadrancopy[x].pop(0)
+            cadrancopy = rotateright(cadrancopy)
+            for x in range(len(liste)//2):
+                for y in range(1,(len(liste)//2)+1):
+                    liste[x][-y] = cadrancopy[x][-y]
+
+
+        if cadran == 3:
+            cadrancopy = copy.deepcopy(liste)
+            for i in range(len(liste)//2):
+               cadrancopy.pop(0)
+            for x in range (len(liste)//2 ):
+                for j in range(len(liste)//2 ):
+                    cadrancopy[x].pop(-1)
+            cadrancopy = rotateright(cadrancopy)
+            for x in range(1,(len(liste)//2)+1):
+                for y in range(len(liste)//2):
+                    liste[-x][y] = cadrancopy[-x][y]
+
+        if cadran == 4:
+            cadrancopy = copy.deepcopy(liste)
+            for i in range(len(liste)//2):
+               cadrancopy.pop(0)
+            for x in range (len(liste)//2 ):
+                for j in range(len(liste)//2 ):
                     cadrancopy[x].pop(0)
-                print(cadrancopy)
-                cadrancopy = rotateright(cadrancopy)
-                liste[2][2] = cadrancopy[0][0]
-                liste[2][3] = cadrancopy[0][1]
-                liste[3][2] = cadrancopy[1][0]
-                liste[3][3] = cadrancopy[1][1]
+            cadrancopy = rotateright(cadrancopy)
+            for x in range(1,(len(liste)//2)+1):
+                for y in range(1,(len(liste)//2)+1):
+                    liste[-x][-y] = cadrancopy[-x][-y]
 
-        else:
-            if cadran == 1:
-                cadrancopy = copy.deepcopy(liste)
-                for i in range(len(liste)//2):
-                    cadrancopy.pop(i-len(liste)//2)
-                    for x in range(len(cadrancopy)//2 +1):
-                        cadrancopy[x].pop(-1)
-                cadrancopy = rotateleft(cadrancopy)
-                liste[0][0] = cadrancopy[0][0]
-                liste[0][1] = cadrancopy[0][1]
-                liste[1][0] = cadrancopy[1][0]
-                liste[1][1] = cadrancopy[1][1]
+    else:
+        if cadran == 1:
+            cadrancopy = copy.deepcopy(liste)
+            for i in range(len(liste)//2):
+               cadrancopy.pop(-1)
+            for x in range (len(liste)//2 ):
+                for j in range(len(liste)//2 ):
+                    cadrancopy[x].pop(-1)
+            cadrancopy = rotateleft(cadrancopy)
+            for x in range(len(liste)//2):
+                for y in range(len(liste)//2):
+                    liste[x][y] = cadrancopy[x][y]
 
-            if cadran == 2:
-                cadrancopy = copy.deepcopy(liste)
-                for i in range(len(liste)//2):
-                    cadrancopy.pop(i-len(liste)//2)
-                    for x in range(len(cadrancopy)//2 +1):
-                        cadrancopy[x].pop(0)
-                cadrancopy = rotateleft(cadrancopy)
-                liste[0][2] = cadrancopy[0][0]
-                liste[0][3] = cadrancopy[0][1]
-                liste[1][2] = cadrancopy[1][0]
-                liste[1][3] = cadrancopy[1][1]
 
-            if cadran == 3:
-                cadrancopy = copy.deepcopy(liste)
-                for i in range(len(liste)//2):
-                    cadrancopy.pop(0)
-                    for x in range(len(cadrancopy)//2 +1):
-                        cadrancopy[x].pop(-1)
-                cadrancopy = rotateleft(cadrancopy)
-                liste[2][0] = cadrancopy[0][0]
-                liste[2][1] = cadrancopy[0][1]
-                liste[3][0] = cadrancopy[1][0]
-                liste[3][1] = cadrancopy[1][1]
-
-            if cadran == 4:
-                cadrancopy = copy.deepcopy(liste)
-                for i in range(len(liste)//2 ):
-                    cadrancopy.pop(0)
-                for x in range(len(liste)//2):
+        if cadran == 2:
+            cadrancopy = copy.deepcopy(liste)
+            for i in range(len(liste)//2):
+               cadrancopy.pop(-1)
+            for x in range (len(liste)//2 ):
+                for j in range(len(liste)//2 ):
                     cadrancopy[x].pop(0)
+            cadrancopy = rotateleft(cadrancopy)
+            for x in range(len(liste)//2):
+                for y in range(1,(len(liste)//2)+1):
+                    liste[x][-y] = cadrancopy[x][-y]
+
+
+        if cadran == 3:
+            cadrancopy = copy.deepcopy(liste)
+            for i in range(len(liste)//2):
+               cadrancopy.pop(0)
+            for x in range (len(liste)//2 ):
+                for j in range(len(liste)//2 ):
+                    cadrancopy[x].pop(-1)
+            cadrancopy = rotateleft(cadrancopy)
+            for x in range(1,(len(liste)//2)+1):
+                for y in range(len(liste)//2):
+                    liste[-x][y] = cadrancopy[-x][y]
+
+        if cadran == 4:
+            cadrancopy = copy.deepcopy(liste)
+            for i in range(len(liste)//2):
+               cadrancopy.pop(0)
+            for x in range (len(liste)//2 ):
+                for j in range(len(liste)//2 ):
                     cadrancopy[x].pop(0)
-                print(cadrancopy)
-                cadrancopy = rotateleft(cadrancopy)
-                liste[2][2] = cadrancopy[0][0]
-                liste[2][3] = cadrancopy[0][1]
-                liste[3][2] = cadrancopy[1][0]
-                liste[3][3] = cadrancopy[1][1]
+            cadrancopy = rotateleft(cadrancopy)
+            for x in range(1,(len(liste)//2)+1):
+                for y in range(1,(len(liste)//2)+1):
+                    liste[-x][-y] = cadrancopy[-x][-y]
 
